f20c83e01de5ad38d414bf2fba5d953bd12016c4
Kyle Powis <kylepowis@Kyles-MacBook-Air.local>
2025-02-11 23:06:06 +0000

    added graphic design poster content

--- components/vfxCarousel.jsx
+++ components/vfxCarousel.jsx
@@ -1,41 +1,35 @@
 "use client";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Play } from "lucide-react";
-import { useState } from "react";
+import { EffectCards, Navigation } from "swiper/modules";
 import "swiper/css";
+import "swiper/css/effect-cards";
 import "swiper/css/navigation";
-import "swiper/css/pagination";
 
 export default function VFXCarousel() {
   const vfx = [
     {
-      title: "TBC",
-      src: "https://example.com/vfx1.mp4",
+      title: "FM Party Comic Poster",
+      src: "https://dasbrosvidepgraphy.s3.eu-north-1.amazonaws.com/FM+PARTY+COMIC+POSTER+FINAL.png",
     },
     {
-      title: "TBC",
-      src: "https://example.com/vfx2.mp4",
+      title: "FM Party Barber Shop",
+      src: "https://dasbrosvidepgraphy.s3.eu-north-1.amazonaws.com/FM+PARTY+BARBER+SHOP.png",
     },
-    // Add more gfx videos as needed
+    {
+      title: "Final FM Party Poster",
+      src: "https://dasbrosvidepgraphy.s3.eu-north-1.amazonaws.com/FINAL+FM+PARTY+POSTER+AMMENDED.png",
+    },
   ];
-
-  const [playing, setPlaying] = useState(null);
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  const handlePlay = (index) => {
-    setPlaying(index);
-  };
 
   return (
     <div className="carousel-container">
       <Swiper
-        spaceBetween={50}
-        slidesPerView={1}
-        navigation
-        pagination={{ clickable: true }}
+        effect="cards"
+        grabCursor={true}
         loop={true}
+        navigation={true}
         className="mySwiper"
-        onSlideChange={({ realIndex }) => setCurrentIndex(realIndex)}
+        modules={[EffectCards, Navigation]}
       >
         {vfx.map((vfxItem, index) => (
           <SwiperSlide key={index}>
@@ -43,24 +37,12 @@ export default function VFXCarousel() {
               <h2 className="absolute top-2 left-2 text-white bg-black bg-opacity-50 px-3 py-1 rounded-md text-sm">
                 {vfxItem.title}
               </h2>
-              {playing === index ? (
-                <video
-                  className="w-full h-full object-cover rounded-xl"
-                  controls
-                  autoPlay
-                >
-                  <source src={vfxItem.src} type="video/mp4" />
-                </video>
-              ) : (
-                <div
-                  className={`relative w-full h-full bg-black bg-opacity-60 rounded-xl flex items-center justify-center cursor-pointer group-hover:opacity-80 transition-opacity ${
-                    currentIndex === index ? "opacity-100" : "opacity-0"
-                  }`}
-                  onClick={() => handlePlay(index)}
-                >
-                  <Play className="w-16 h-16 text-white opacity-80 transition-opacity group-hover:opacity-100" />
-                </div>
-              )}
+
+              <img
+                src={vfxItem.src}
+                alt={vfxItem.title}
+                className="w-full h-full object-cover rounded-xl"
+              />
             </div>
           </SwiperSlide>
         ))}
