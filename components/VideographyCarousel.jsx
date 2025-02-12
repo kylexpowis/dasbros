f70637a654228654498ed8dd9acd67ed0def9c1d
Kyle Powis <kylepowis@Kyles-MacBook-Air.local>
2025-02-12 01:56:03 +0000

    add thumbnails to videography section

--- components/VideographyCarousel.jsx
+++ components/VideographyCarousel.jsx
@@ -12,54 +12,80 @@ export default function VideoCarousel() {
     {
       title: "FM Party - Dont Be Late",
       src: "https://dasbrosvidepgraphy.s3.eu-north-1.amazonaws.com/FM+PARTY+-+DONT+BE+LATE+COMPLETE+2.mp4",
+      thumbnail:
+        "https://dasbrosvidepgraphy.s3.eu-north-1.amazonaws.com/fmpartydontbelatethumbnail.png",
     },
     {
       title: "FM Party - The Link Up",
       src: "https://dasbrosvidepgraphy.s3.eu-north-1.amazonaws.com/final+fm+party+the+link+up.mp4",
+      thumbnail:
+        "https://dasbrosvidepgraphy.s3.eu-north-1.amazonaws.com/fmpartythelinkupthumbnail.png",
     },
     {
       title: "GEEKFILES",
       src: "https://dasbrosvidepgraphy.s3.eu-north-1.amazonaws.com/DASBROS+GEEKFILES+COMPETITION.mp4",
+      thumbnail:
+        "https://dasbrosvidepgraphy.s3.eu-north-1.amazonaws.com/geekfilesthumbnail.png",
     },
     {
       title: "FM Party - The Drop Off",
       src: "https://dasbrosvidepgraphy.s3.eu-north-1.amazonaws.com/FM+PARTY+3+-+THE+DROP+OFF.mp4",
+      thumbnail:
+        "https://dasbrosvidepgraphy.s3.eu-north-1.amazonaws.com/FM+PARTY+3+-+THE+DROP+OFF.mp4",
     },
     {
       title: "FM Party Ad",
       src: "https://dasbrosvidepgraphy.s3.eu-north-1.amazonaws.com/FM+PARTY+AD+4+COMPLETE+FINAL.mp4",
+      thumbnail:
+        "https://dasbrosvidepgraphy.s3.eu-north-1.amazonaws.com/fmpartyad4thumbnail.png",
     },
     {
       title: "FM Party Ad 2",
       src: "https://dasbrosvidepgraphy.s3.eu-north-1.amazonaws.com/FM+PARTY+ADVERT+COMPLETE.mp4",
+      thumbnail:
+        "https://dasbrosvidepgraphy.s3.eu-north-1.amazonaws.com/fmpartyadcompletethumbnail.png",
     },
     {
       title: "July 7 - You Like It",
       src: "https://dasbrosvidepgraphy.s3.eu-north-1.amazonaws.com/July+7++-+You+Like+It+Master+(COMPLETE).mp4",
+      thumbnail:
+        "https://dasbrosvidepgraphy.s3.eu-north-1.amazonaws.com/july7youlikeit.png",
     },
     {
       title: "Ken Carson",
       src: "https://dasbrosvidepgraphy.s3.eu-north-1.amazonaws.com/KEN+CARSON+complete.mp4",
+      thumbnail:
+        "https://dasbrosvidepgraphy.s3.eu-north-1.amazonaws.com/kencarsonthumbnail.png",
     },
     {
       title: "Sainte - Leeds Fest 2023",
       src: "https://dasbrosvidepgraphy.s3.eu-north-1.amazonaws.com/LEEDS+FESTIVAL+SAINTE+2023.mp4",
+      thumbnail:
+        "https://dasbrosvidepgraphy.s3.eu-north-1.amazonaws.com/sainteleedsfestthumbnail.png",
     },
     {
       title: "Meekz - Leeds Fest 2023",
       src: "https://dasbrosvidepgraphy.s3.eu-north-1.amazonaws.com/MEEKZ+Leeds+Festival.mp4",
+      thumbnail:
+        "https://dasbrosvidepgraphy.s3.eu-north-1.amazonaws.com/meekzleedsfestthumbnail.png",
     },
     {
       title: "Skitzo - Wear The Grit",
       src: "https://dasbrosvidepgraphy.s3.eu-north-1.amazonaws.com/SKITZO+-+WEAR+THE+GRIT+-+DIR+FINAL.mp4",
+      thumbnail:
+        "https://dasbrosvidepgraphy.s3.eu-north-1.amazonaws.com/SKITZO+-+WEAR+THE+GRIT+-+DIR+FINAL.mp4",
     },
     {
       title: "July 7 - Don't move too fast",
       src: "https://dasbrosvidepgraphy.s3.eu-north-1.amazonaws.com/July+7+-+Don't+Move+Too+Fast.mp4",
+      thumbnail:
+        "https://dasbrosvidepgraphy.s3.eu-north-1.amazonaws.com/july7dontmovetoofast.png",
     },
     {
       title: "July 7 - Just Imagine",
       src: "https://dasbrosvidepgraphy.s3.eu-north-1.amazonaws.com/JULY+7+JUST+IMAGINE+complete+video+.mp4",
+      thumbnail:
+        "https://dasbrosvidepgraphy.s3.eu-north-1.amazonaws.com/july7justimaginethumbnail.png",
     },
   ];
 
@@ -79,7 +105,7 @@ export default function VideoCarousel() {
         navigation={true}
         className="mySwiper"
         modules={[EffectCards, Navigation]}
-        onSlideChange={({ realIndex }) => setCurrentIndex(realIndex)} // Update the index on slide change
+        onSlideChange={({ realIndex }) => setCurrentIndex(realIndex)}
       >
         {videos.map((video, index) => (
           <SwiperSlide key={index}>
@@ -87,6 +113,7 @@ export default function VideoCarousel() {
               <h2 className="absolute top-2 left-2 text-white bg-black bg-opacity-50 px-3 py-1 rounded-md text-sm">
                 {video.title}
               </h2>
+
               {playing === index ? (
                 <video
                   className="w-full h-full object-cover rounded-xl"
@@ -96,14 +123,12 @@ export default function VideoCarousel() {
                   <source src={video.src} type="video/mp4" />
                 </video>
               ) : (
-                <div
-                  className={`relative w-full h-full bg-black bg-opacity-60 rounded-xl flex items-center justify-center cursor-pointer group-hover:opacity-80 transition-opacity ${
-                    currentIndex === index ? "opacity-100" : "opacity-0"
-                  }`}
+                <img
+                  src={video.thumbnail}
+                  alt={video.title}
+                  className="w-full h-full object-cover rounded-xl cursor-pointer"
                   onClick={() => handlePlay(index)}
-                >
-                  <Play className="w-16 h-16 text-white opacity-80 transition-opacity group-hover:opacity-100" />
-                </div>
+                />
               )}
             </div>
           </SwiperSlide>
